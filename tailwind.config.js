a356a12eff4bc8f3243cce754a1d6c61833de1b5
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-09-05 20:17:23 +0100

    ♻️ Refactor CSS into tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         'accent-3': '#F6D474',
       },
       backgroundImage: {
-        'hero-image': "url(/assets/background-desktop.jpeg)"
+        'hero-image': 'url(/assets/background-desktop.jpeg)',
       },
       spacing: {
         28: '7rem',
