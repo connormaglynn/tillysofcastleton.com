329dbcc6d995533c160206f0fc842a07d2d5eaec
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-09-06 19:53:34 +0100

    🔥 Remove unused tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,29 +10,15 @@ module.exports = {
       backgroundImage: {
         'hero-image': 'url(/assets/background-desktop.jpeg)',
       },
-      spacing: {
-        28: '7rem',
-      },
-      letterSpacing: {
-        tighter: '-.04em',
-      },
-      lineHeight: {
-        tight: 1.2,
+      fontFamily: {
+        brand: "'The Girl Next Door', cursive",
+        brand2: "'Single Day', cursive",
       },
       fontSize: {
         '5xl': '2.5rem',
         '6xl': '2.75rem',
         '7xl': '4.5rem',
         '8xl': '6.25rem',
-      },
-      fontFamily: {
-        mono: ['Roboto'],
-        brand: "'The Girl Next Door', cursive",
-        brand2: "'Single Day', cursive",
-      },
-      boxShadow: {
-        sm: '0 5px 10px rgba(0, 0, 0, 0.12)',
-        md: '0 8px 30px rgba(0, 0, 0, 0.12)',
       },
     },
   },
